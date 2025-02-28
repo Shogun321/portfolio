947a217061e02d7e6fbb9b52c29f2a386403a207
Shogun321 <bojanic.vladinir@gmail.com>
2025-02-28 14:10:05 +0100

    update2

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-// testing github
+// testing github without gh pages
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
